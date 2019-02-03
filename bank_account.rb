3dad996c3c6e92a61cd26edd1fd5b388cf0e8388
Daramola Ajiboye <daramolaajibigad@gmail.com>
2019-02-03 23:15:34 +0100

    "Adds print_register method for bank account class"

--- bank_account.rb
+++ bank_account.rb
@@ -27,6 +27,20 @@ class BankAccount
       return balance
     end
     
+    def print_register
+        puts "Printing Register"
+        puts "-" * 40
+        puts "Name: #{@name}"
+        puts "-" * 40
+        puts "#{"Description".ljust(30)}#{"Amount".rjust(10)}"
+        puts "-" * 40
+      @transactions.each do |transaction|
+        puts "#{transaction[:description].ljust(30)}#{sprintf("%0.2f", transaction[:amount]).rjust(10)}"
+      end
+      puts "-" * 40
+      puts "#{"Balance:".ljust(30)}#{sprintf("%0.2f", balance).rjust(10)}"
+    end
+
     def to_s
       "Name: #{name}, Balance: #{sprintf("%0.2f", balance)}"
     end
@@ -36,4 +50,5 @@ class BankAccount
   bank_account.credit("Paycheck", 100)
   bank_account.debit("Groceries", 40)
   puts bank_account.balance
+  bank_account.print_register
   
